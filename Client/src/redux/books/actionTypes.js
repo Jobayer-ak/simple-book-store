795162dbac10fcd912cbea2a6a1b1b3d6fe9ea96
Jobayer-ak <jobayer.ak@gmail.com>
2023-11-25 16:52:34 +0600

    PROCESSING: working on redux. Featue is book

---
+++ Client/src/redux/books/actionTypes.js
@@ -0,0 +1,5 @@
+// action types
+export const ADDBOOK = "books/addBook";
+export const UPDATEBOOK = "books/updateBook";
+export const DELETEBOOK = "books/deleteBook";
+export const TOGGLEBOOKS = "books/toggleBooks";
